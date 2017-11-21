08e2431ab46ad7a6f6ed02d5cec132fcce4b7f40
iEnder <pkmn2024@gmail.com>
2017-11-20 20:56:27 -0500

    dev setup

--- start.js
+++ start.js
@@ -10,13 +10,6 @@ mongoose.connection.on('error', err => {
     console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
-// READY?! Let's go!
-
-// import models
-require('./models/Store');
-require('./models/User');
-require('./models/Review');
-
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
