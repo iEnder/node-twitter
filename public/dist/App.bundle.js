79b0fca63c1b2b26bd166c3627da2585f2ef9823
iEnder <pkmn2024@gmail.com>
2017-12-03 17:12:29 -0500

    fixed multiple dropdowns being open at once

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -166,13 +166,22 @@ Object.defineProperty(exports, "__esModule", {
 });
 function attachDropdownToButton(button, dropdown) {
   button.addEventListener('click', function (e) {
+    // find and hide current dropdown
+    var current = document.querySelector('.dropdown__menu--active');
+    if (current && current != dropdown) {
+      current.classList.remove('dropdown__menu--active');
+    }
+
+    // toggle dropdown menu
     dropdown.classList.toggle('dropdown__menu--active');
   });
 }
 
 function bindDropdowns(selector) {
+  // get all dropdowns from given selector
   var dropdowns = document.querySelectorAll(selector);
 
+  // bind the button to the menu with attach function
   dropdowns.forEach(function (dropdown) {
     var dropdownBtn = dropdown.querySelector('.dropdown__toggle-btn');
     var dropdownMenu = dropdown.querySelector('.dropdown__menu');
--- public/javascripts/modules/dropdowns.js
+++ public/javascripts/modules/dropdowns.js
@@ -1,12 +1,21 @@
 function attachDropdownToButton(button, dropdown) {
   button.addEventListener('click', function(e) {
+    // find and hide current dropdown
+    const current = document.querySelector('.dropdown__menu--active');
+    if (current && current != dropdown) {
+      current.classList.remove('dropdown__menu--active');
+    }
+
+    // toggle dropdown menu
     dropdown.classList.toggle('dropdown__menu--active');
   });
 }
 
 function bindDropdowns(selector) {
+  // get all dropdowns from given selector
   const dropdowns = document.querySelectorAll(selector);
 
+  // bind the button to the menu with attach function
   dropdowns.forEach(dropdown => {
     const dropdownBtn = dropdown.querySelector('.dropdown__toggle-btn');
     const dropdownMenu = dropdown.querySelector('.dropdown__menu');
